5cace68797b5bc9a0108f953e878f466300524cc
sil <git@sil.dev>
2023-08-23 01:16:39 -0500

    Maybe we should be using sequelize instead...

---
+++ src/services/db.js
@@ -0,0 +1,9 @@
+// db.js
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+module.exports = sequelize;
