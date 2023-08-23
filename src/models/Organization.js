f3e25e716bd81f48a5a8c08d3f37e32fdc7c9902
sil <git@sil.dev>
2023-08-23 01:16:29 -0500

    Some sample code for organizations

--- src/models/Organization.js
+++ src/models/Organization.js
@@ -1,8 +1,57 @@
-class Organization {
-    constructor(name, owningCharacter) {
+class OrganizationRole {
+    constructor(id, name, organization) {
+        this.id = id;
         this.name = name;
-        this.owningCharacter = owningCharacter;
+        this.organization = organization;
+        this.permissions = [];
+    }
+}
+
+class OrganizationMember {
+    constructor(organization, character, role) {
+        this.organization = organization;
+        this.character = character;
+        this.role = role;
+    }
+}
+
+class Organization {
+    constructor(name, owner) {
+        this.name = name;
+        this.owner = owner; // Character
+
         this.members = [];
+        this.roles = {};
+
+        this.roles["owner"] = new OrganizationRole("owner", 'Owner', this);
+        this.roles["admin"] = new OrganizationRole("admin", 'Admin', this);
+        this.roles["member"] = new OrganizationRole("member", 'Member', this);
+        this.roles["recruit"] = new OrganizationRole("recruit", 'Recruit', this);
+
+        this.addMember(owner, "owner");
+    }
+
+    addMember(character, roleID) {
+        let role = this.roles[roleID];
+
+        if (!role) {
+            throw new Error('Invalid role ID.');
+        }
+
+        this.members.push(new OrganizationMember(this, character, role));
+    }
+
+    removeMember(character) {
+        // Disallow removing the owner
+        if (character === this.owner) {
+            throw new Error('Cannot remove the owner from the organization.');
+        }
+
+        this.members = this.members.filter(m => m.character !== character);
+    }
+
+    disband() {
+        // @TODO
     }
 }
 
